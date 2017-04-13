f97e7ff51e13b41babec22d994f7442b3a3fe443
LyuShiWang <nplxdsy@gmail.com>
2017-04-13 15:37:50 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/lyushiwang/netobserve/setting_weather.java
@@ -0,0 +1,10 @@
+package com.lyushiwang.netobserve;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by win10 on 2017/4/12.
+ */
+
+public class setting_weather extends AppCompatActivity {
+}
