20624698b5957d7fb2187f4d6760a8f885c9ebf0
吕世望 <nplxdsy@gmail.com>
2017-04-22 17:13:02 +0800

    observe_now

---
+++ app/src/main/java/com/lyushiwang/netobserve/observe_now.java
@@ -0,0 +1,17 @@
+package com.lyushiwang.netobserve;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 吕世望 on 2017/4/22.
+ */
+
+public class observe_now extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.observe_now);
+
+    }
+}
