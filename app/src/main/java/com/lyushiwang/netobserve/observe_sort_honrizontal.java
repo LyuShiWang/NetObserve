409ac605a7473ad05ca1a0107688998ec207177d
吕世望 <nplxdsy@gmail.com>
2017-05-01 22:36:08 +0800

    PC

---
+++ app/src/main/java/com/lyushiwang/netobserve/observe_sort_honrizontal.java
@@ -0,0 +1,17 @@
+package com.lyushiwang.netobserve;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 吕世望 on 2017/5/1.
+ */
+
+public class observe_sort_honrizontal extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.observe_sort_honrizontal);
+    }
+}
---
+++ app/src/main/java/com/lyushiwang/netobserve/observe_sort_vertical.java
@@ -0,0 +1,16 @@
+package com.lyushiwang.netobserve;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 吕世望 on 2017/5/1.
+ */
+
+public class observe_sort_vertical extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.observe_sort_vertical);
+    }
+}
