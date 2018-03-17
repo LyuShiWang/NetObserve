4219c30a2ac974e150689d9e3ffffc9e1463dd41
吕世望 <nplxdsy@gmail.com>
2018-03-17 19:37:29 +0800

    PC

--- app/src/main/java/com/lyushiwang/netobserve/observe/observe_sort_vertical.java
+++ app/src/main/java/com/lyushiwang/netobserve/observe/observe_sort_vertical.java
@@ -3,6 +3,7 @@ package com.lyushiwang.netobserve.observe;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.ComponentName;
@@ -61,7 +62,9 @@ public class observe_sort_vertical extends AppCompatActivity {
     private ImageButton imageButton_houtui;
 
     private My_Func my_func = new My_Func();
-    private  String ProName=new String();
+    private String ProName = new String();
+
+    private Dialog dialog_tip;
 
     private ClassMeasFunction classmeasFun;//GeoCom
     private BluetoothAdapter BluetoothAdap;// 本地蓝牙适配器
@@ -109,58 +112,58 @@ public class observe_sort_vertical extends AppCompatActivity {
         button_tranfer.setOnClickListener(listener_transfer);
     }
 
-    protected void define_palettes(){
-        editText_in1_name=(EditText) findViewById(R.id.editText_in1_name);
-        textView_in1_text=(TextView)findViewById(R.id.textView_in1_text);
-        button_check=(Button)findViewById(R.id.button_check);
-        button_get_vert_data=(Button)findViewById(R.id.button_get_vert_data);
-        button_tranfer=(Button)findViewById(R.id.button_transfer);
-        imageButton_houtui=(ImageButton)findViewById(R.id.imageButton_houtui);
+    protected void define_palettes() {
+        editText_in1_name = (EditText) findViewById(R.id.editText_in1_name);
+        textView_in1_text = (TextView) findViewById(R.id.textView_in1_text);
+        button_check = (Button) findViewById(R.id.button_check);
+        button_get_vert_data = (Button) findViewById(R.id.button_get_vert_data);
+        button_tranfer = (Button) findViewById(R.id.button_transfer);
+        imageButton_houtui = (ImageButton) findViewById(R.id.imageButton_houtui);
     }
 
     //初始化
     @SuppressLint("NewApi")
-    private void init(){
+    private void init() {
         BluetoothAdap = BluetoothAdapter.getDefaultAdapter();// 获取本地蓝牙适配器
         bindContactService();
-        ProName=null;
+        ProName = null;
     }
 
-    public void givetip(){
-        AlertDialog.Builder AD_tip=new AlertDialog.Builder(observe_sort_vertical.this);
+    public void givetip() {
+        AlertDialog.Builder AD_tip = new AlertDialog.Builder(observe_sort_vertical.this);
         AD_tip.setTitle("提示").setMessage("输入工程名后，点击“确定”。然后在水准仪上发送数据，提示“接收成功”后点击“获取数据”");
-        AlertDialog adg_tip=AD_tip.show();
+        AlertDialog adg_tip = AD_tip.show();
     }
 
-    Button.OnClickListener listener_check=new Button.OnClickListener(){
-        public void onClick(View v){
-            ProName=editText_in1_name.getText().toString();
-            if (ProName.equals("")||ProName.equals(null)){
-                AlertDialog.Builder AD_error=new AlertDialog.Builder(observe_sort_vertical.this);
-                AlertDialog adg_error=AD_error.setTitle("警告").setMessage("工程名出错！请检查后重新输入").show();
-            }else{
+    Button.OnClickListener listener_check = new Button.OnClickListener() {
+        public void onClick(View v) {
+            ProName = editText_in1_name.getText().toString();
+            if (ProName.equals("") || ProName.equals(null)) {
+                AlertDialog.Builder AD_error = new AlertDialog.Builder(observe_sort_vertical.this);
+                AlertDialog adg_error = AD_error.setTitle("警告").setMessage("工程名出错！请检查后重新输入").show();
+            } else {
                 classmeasFun.CleanData();
                 makeToast("输入工程名成功！");
             }
         }
     };
 
-    Button.OnClickListener listener_get=new Button.OnClickListener(){
+    Button.OnClickListener listener_get = new Button.OnClickListener() {
         public void onClick(View v) {
             if (!BluetoothAdap.isEnabled()) {
                 android.app.AlertDialog.Builder AD_check_BT = new android.app.AlertDialog.Builder(
                         observe_sort_vertical.this);
-                AlertDialog adg_check_BT= AD_check_BT.setMessage("未打开蓝牙！请打开！").show();
+                AlertDialog adg_check_BT = AD_check_BT.setMessage("未打开蓝牙！请打开！").show();
             } else {
-                AlertDialog.Builder AD_receive=new AlertDialog.Builder(observe_sort_vertical.this);
-                AlertDialog adg_receive= AD_receive.setMessage("正在接收文件...").show();
-                String data=new String();
-                try{
+                AlertDialog.Builder AD_receive = new AlertDialog.Builder(observe_sort_vertical.this);
+                AlertDialog adg_receive = AD_receive.setMessage("正在接收文件...").show();
+                String data = new String();
+                try {
                     data = classmeasFun.receiveData();
                     adg_receive.dismiss();
 
                     textView_in1_text.setText("");
-                    textView_in1_text.setText(ProName+".GSI\r\n"+data);
+                    textView_in1_text.setText(ProName + ".GSI\r\n" + data);
                 } catch (Exception e) {
                     e.printStackTrace();
                     android.app.AlertDialog.Builder AD_check_measfun = new android.app.AlertDialog.Builder(observe_sort_vertical.this);
@@ -176,68 +179,130 @@ public class observe_sort_vertical extends AppCompatActivity {
                             }).create().show();
                 }
 
-                if (!data.equals(null)&&!data.equals("")){
-                    File filefolder_in1=new File(my_func.get_main_file_path(),ProName);
-                    if (!filefolder_in1.exists()){
+                if (!data.equals(null) && !data.equals("")) {
+                    File filefolder_in1 = new File(my_func.get_main_file_path(), ProName);
+                    if (!filefolder_in1.exists()) {
                         filefolder_in1.mkdir();
                     }
 
-                    File file_in1=new File(my_func.get_main_file_path()+"/"+ProName,
-                            ProName+".GSI");
-                    try{
-                        if (!file_in1.exists()){
-                            file_in1.createNewFile();
+                    File file_gsi = new File(my_func.get_main_file_path() + "/" + ProName,
+                            ProName + ".GSI");
+                    try {
+                        if (!file_gsi.exists()) {
+                            file_gsi.createNewFile();
                         }
-                        BufferedWriter bw=new BufferedWriter(new FileWriter(file_in1,true));
-                        bw.flush();bw.write(data);bw.flush();bw.close();
-                    }catch (IOException e){
+                        BufferedWriter bw = new BufferedWriter(new FileWriter(file_gsi, true));
+                        bw.flush();
+                        bw.write(data);
+                        bw.flush();
+                        bw.close();
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }else{
-                    AlertDialog.Builder AD_data_error=new AlertDialog.Builder(observe_sort_vertical.this);
-                    AlertDialog adg_data_error=AD_data_error.setTitle("警告").setMessage("接收的数据出现问题！").show();
+                } else {
+                    AlertDialog.Builder AD_data_error = new AlertDialog.Builder(observe_sort_vertical.this);
+                    AlertDialog adg_data_error = AD_data_error.setTitle("警告").setMessage("接收的数据出现问题！").show();
                 }
             }
         }
     };
 
-    Button.OnClickListener listener_transfer=new Button.OnClickListener() {
+    Button.OnClickListener listener_transfer = new Button.OnClickListener() {
         public void onClick(View v) {
-            final AlertDialog.Builder AD_transfer=new AlertDialog.Builder(observe_sort_vertical.this);
-            AD_transfer.setMessage("是否将"+ProName+".GSI文件转为.in1文件？")
-                    .setPositiveButton("确定",new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    ProgressDialog PD_transfer=new ProgressDialog(observe_sort_vertical.this);
-
-                    PD_transfer.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                    PD_transfer.setTitle("提示");
-                    PD_transfer.setCanceledOnTouchOutside(false);//dialog弹出后会点击屏幕，dialog不消失；点击物理返回键dialog消失
-                    PD_transfer.setMessage("正在转换格式，请等待......");
-                    PD_transfer.setOnDismissListener(new DialogInterface.OnDismissListener(){
+            final AlertDialog.Builder AD_transfer = new AlertDialog.Builder(observe_sort_vertical.this);
+            AD_transfer.setMessage("是否将" + ProName + ".GSI文件转为.in1文件？")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            String filename=ProName+".gsi";
-                            if(transfer_data(filename)){
-                                AlertDialog.Builder AD_true=new AlertDialog.Builder(observe_sort_vertical.this);
-                                AD_true.setTitle("提示").setMessage("转换成功！")
-                                        .setPositiveButton("确定",new DialogInterface.OnClickListener(){
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
+                        public void onClick(DialogInterface dialog, int which) {
+                            ProgressDialog PD_transfer = new ProgressDialog(observe_sort_vertical.this);
 
-                                            }
-                                        });
-                            }else{
-
-                            }
+                            PD_transfer.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                            PD_transfer.setTitle("提示");
+                            PD_transfer.setCanceledOnTouchOutside(false);//dialog弹出后会点击屏幕，dialog不消失；点击物理返回键dialog消失
+                            PD_transfer.setMessage("正在转换格式，请等待......");
+                            PD_transfer.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                                @Override
+                                public void onDismiss(DialogInterface dialog) {
+                                    String filename = ProName + ".gsi";
+                                    if (transfer_data(filename)) {
+                                        dialog_tip = new AlertDialog.Builder(observe_sort_vertical.this)
+                                                .setTitle("提示")
+                                                .setMessage("转换成功！")
+                                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        dialog_tip.dismiss();
+                                                    }
+                                                }).create();
+                                        dialog_tip.show();
+                                    } else {
+                                        dialog_tip = new AlertDialog.Builder(observe_sort_vertical.this)
+                                                .setTitle("提示")
+                                                .setMessage("转换失败，请检查")
+                                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        dialog_tip.dismiss();
+                                                    }
+                                                }).create();
+                                        dialog_tip.show();
+                                    }
+                                }
+                            });
                         }
-                    });
-                }}).show();
+                    }).show();
         }
     };
 
-    public boolean transfer_data(String GSIFileName){
-        boolean istransfered=false;
+    public boolean transfer_data(String GSIFileName) {
+        boolean istransfered = false;
+
+        //File file_gsi=new File(my_func.get_main_file_path()+"/"+ProName,
+        //      ProName+".GSI");
+
+        //test
+        File file_gsi = new File(my_func.get_main_file_path() + "/" + "0802",
+                "0802.GSI");
+
+        StringBuffer knowing_points = new StringBuffer();//已知点
+
+        try {
+            BufferedReader gsi_reader = new BufferedReader(new FileReader(file_gsi));
+            String read_line = "";
+            StringBuffer Code_Block = new StringBuffer();
+            while ((read_line = gsi_reader.readLine()) != null) {
+                String first_data_word = read_line.split(" ")[0];
+                String Word_Index = first_data_word.substring(0, 2);
+                //String row_number=first_data_word.substring(3,first_data_word.length());
+                if (Word_Index == "41") {
+                    Integer row_number = Integer.valueOf(first_data_word.substring(3, first_data_word.length()));
+                    if (row_number != 1) {
+                        Code_Block = new StringBuffer();
+                    }
+                }
+                Code_Block.append(read_line);
+
+                String name_code=first_data_word.substring(7,first_data_word.length());
+                String point_name=name_code.replaceFirst("0*","");//去掉左边的零
+
+                String second_data_word=read_line.split("")[1];
+                Word_Index=second_data_word.substring(0,2);
+                if (Word_Index=="83"){//第二个Data word information的开头是“83”，是已知点
+                    String height_code=second_data_word.substring(7,second_data_word.length());
+                    String knowing_height=height_code.replaceFirst("^0*", "");//去掉左边的零
+                    if (knowing_height==""){
+                        knowing_height="0";
+                    }
+
+                    knowing_points.append(point_name+","+knowing_height);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            makeToast("读取.gsi文件出错！");
+        }
+
+        String write_content = new String();
 
         return istransfered;
     }
