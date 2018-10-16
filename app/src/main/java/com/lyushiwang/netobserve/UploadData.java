ea1de0f6820375e036074653e01ede639d5d30d4
吕世望 <nplxdsy@gmail.com>
2018-10-16 11:26:56 +0800

    PC

--- app/src/main/java/com/lyushiwang/netobserve/UploadData.java
+++ app/src/main/java/com/lyushiwang/netobserve/UploadData.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -62,7 +63,7 @@ public class UploadData extends AppCompatActivity implements AdapterView.OnItemC
         //        String text="本机IP地址为："+ localAddress +"\n"+
         //                "本机IP地址前缀为："+Index+"\n"+
         //                "本机设备名为："+Name;
-        AlertDialog.Builder AD_IPAddress=new AlertDialog.Builder(UploadData.this);
+        AlertDialog.Builder AD_IPAddress = new AlertDialog.Builder(UploadData.this);
         //        AD_IPAddress.setMessage(text).setPositiveButton("确定",null).create().show();
 
         MsgHandler = new Handler() {
@@ -70,7 +71,7 @@ public class UploadData extends AppCompatActivity implements AdapterView.OnItemC
             public void handleMessage(Message msg) {
                 if (msg.what == 1) {
                     makeToast("平差完毕！请接收.ou2文件！");
-                    AlertDialog.Builder AD_check=new AlertDialog.Builder(UploadData.this);
+                    AlertDialog.Builder AD_check = new AlertDialog.Builder(UploadData.this);
                     AD_check.setMessage("平差完毕！请接收.ou2文件！").show();
                 }
                 if (msg.what == 2) {
@@ -118,8 +119,13 @@ public class UploadData extends AppCompatActivity implements AdapterView.OnItemC
         file_in2 = new File(my_functions.get_main_file_path() + "/" + ProjectName, ProjectName + ".in2");
         if (file_in2.exists()) {
             //将该in2文件上传即可
-            connect_PC(ProjectName);
-            wait4checked();
+            //connect_PC(ProjectName);
+
+            Thread connect = new Thread(new connectPC());
+            connect.start();
+
+            Thread check_thread = new Thread(new CheckThread());
+            check_thread.start();
         } else {
             makeToast("没有找到in2文件！");
         }
@@ -149,7 +155,7 @@ public class UploadData extends AppCompatActivity implements AdapterView.OnItemC
             public void run() {
                 try {
                     //1.创建客户端Socket，指定服务器地址和端口
-                    String ServeIPAdress="10.6.0.54";
+                    String ServeIPAdress = "10.6.0.54";
                     Socket socket = new Socket(ServeIPAdress, 8884);
                     //2.获取输出流，向服务器端发送信息
                     OutputStream os = socket.getOutputStream();//字节输出流
@@ -158,7 +164,7 @@ public class UploadData extends AppCompatActivity implements AdapterView.OnItemC
                     //获取客户端的IP地址
                     //                    InetAddress address = InetAddress.getLocalHost();
                     //                    String ip = address.getHostAddress();
-                    pw.write(ProjectName+"\n");
+                    pw.write(ProjectName + "\n");
                     try {
                         String line;
                         BufferedReader bf = new BufferedReader(new FileReader(file_in2));
@@ -174,46 +180,108 @@ public class UploadData extends AppCompatActivity implements AdapterView.OnItemC
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-                Thread check_thread=new Thread(new CheckThread());
-                try {
-                    Thread.sleep(1000);
-                }catch (Exception e){
-
-                }
-                check_thread.start();
             }
         }.start();
     }
 
-    class CheckThread implements Runnable{
-        public void run(){
-            try{
-                //等待服务器传回“平差完毕”的讯号
-                String ServeIPAdress="10.6.0.54";
-                Socket socket_check = new Socket(ServeIPAdress, 8886);
-                BufferedReader br_check=new BufferedReader(new InputStreamReader(socket_check.getInputStream()));
-
-                String ischecked=br_check.readLine();
-                Message msg=new Message();
-                if (ischecked.equals("checked")){
-                    msg.what=1;
-                    MsgHandler.sendMessage(msg);
-                }else{
-                    msg.what=2;
-                    MsgHandler.sendMessage(msg);
+    class connectPC implements Runnable {
+        @Override
+        public void run() {
+            try {
+                //1.创建客户端Socket，指定服务器地址和端口
+                String ServeIPAdress = "10.6.0.54";
+                Socket socket = new Socket(ServeIPAdress, 8884);
+                //2.获取输出流，向服务器端发送信息
+                OutputStream os = socket.getOutputStream();//字节输出流
+                PrintWriter pw = new PrintWriter(os);//将输出流包装为打印流
+                pw = new PrintWriter(socket.getOutputStream());
+                //获取客户端的IP地址
+                //                    InetAddress address = InetAddress.getLocalHost();
+                //                    String ip = address.getHostAddress();
+                pw.write(ProjectName + "\n");
+                try {
+                    String line;
+                    BufferedReader bf = new BufferedReader(new FileReader(file_in2));
+                    while (((line = bf.readLine()) != null)) {
+                        pw.write(line + "\n");
+                    }
+                } catch (Exception e) {
                 }
-            }catch (Exception e){
+                pw.flush();//刷新打印流，这样就把pw中的数据发送到服务器端
+
+                Thread check_thread=new Thread(new CheckThread());
+                check_thread.start();
+//
+//                Thread testthread=new Thread(new TestThread());
+//                testthread.start();
+
+                pw.close();//关闭打印流
+                socket.close();//关闭套接字
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class CheckThread implements Runnable {
+        public void run() {
+            int i=0;
+            label1:while(true) {
+                try {
+                    Log.i("Worning:", "CheckThread线程正在运行 "+String.valueOf(i));
+                    i=i+1;
+                    Thread.sleep(1000);
+
+                    //等待服务器传回“平差完毕”的讯号
+                    String ServeIPAdress = "10.6.0.54";
+                    Socket socket_check = new Socket(ServeIPAdress, 12345);
+                    BufferedReader br_check = new BufferedReader(new InputStreamReader(socket_check.getInputStream()));
+
+                    String ischecked = "";
+
+                    while ((ischecked = br_check.readLine()) != null) {
+                        Log.i("Tips:", "已连接，已接收到反馈信息");
+                        Message msg = new Message();
+                        if (ischecked.equals("checked")) {
+                            msg.what = 1;
+                            MsgHandler.sendMessage(msg);
+                            break label1;
+                        } else {
+                            msg.what = 2;
+                            MsgHandler.sendMessage(msg);
+                            break label1;
+                        }
+                    }
+                } catch (Exception e) {
+                }
+
+                if (i>10){
+                    break label1;
+                }
             }
         }
     }
-    public void wait4checked(){
-        new Thread(){
-            @Override
-            public void run(){
+
+    class TestThread implements Runnable{
+        @Override
+        public void run() {
+            try{
+                int i=0;
+                while(true){
+                    Log.i("Test","Testing"+String.valueOf(i));
+                    Thread.sleep(1000);
+                    i=i+1;
+
+                    if(i>1000){
+                        break;
+                    }
+                }
+            }catch (Exception e){
 
             }
-        }.start();
+        }
     }
 
     public void makeToast(String text) {
